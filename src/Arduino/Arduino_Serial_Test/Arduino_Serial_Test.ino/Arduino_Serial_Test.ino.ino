e1e1774ea5b82d3fca30a43332dc02bf18b2b124
Andrew Thompson <athom058@gold.ac.uk>
2017-08-15 10:52:05 +0100

    Add some project docs

--- src/Arduino/Arduino_Serial_Test/Arduino_Serial_Test.ino/Arduino_Serial_Test.ino.ino
+++ src/Arduino/Arduino_Serial_Test/Arduino_Serial_Test.ino/Arduino_Serial_Test.ino.ino
@@ -20,26 +20,28 @@ int grid_2[64] = {
   1, 0, 0, 0, 0, 0, 0, 1,
 };
 
+// Pointer used to set which grid to output
 int * grid;
 
 void setup() {
-  // Put your setup code here, to run once:
   Serial.begin(9600);
 
   grid = grid_1;
 }
 
 void loop() {
+  // Change grid every 5 seconds
+  // This assigns the pointer every loop to simulate reading the real grid
   if (millis() % 10000 > 5000) {
     grid = grid_2;
   } else {
     grid = grid_1;
   }
   
-  // Put your main code here, to run repeatedly:
   if (Serial.available() > 0) {
     byte byteRead = Serial.read();
     switch (byteRead) {
+      // oF requests a grid update
       case 255:
         for(int i = 0; i < 64; i++) {
           Serial.write(grid[i]);
